cbc2ab15f1ca1c6b5fe34a10865f41d703d3316d
Arie Darmawan <ardrmwn.git@gmail.com>
2023-08-20 00:55:30 +0800

    temanrobot v.1

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Anto Naim",
+    username: "Wakasinaim",
+    email: "MbaAh@ayam.top",
+  },
+  {
+    id: 2,
+    name: "Edy Purno",
+    username: "Edyporn",
+    email: "Pris@nasgorhujan.tv",
+  },
+  {
+    id: 3,
+    name: "Derya Roswana",
+    username: "PdipJaya",
+    email: "widwid@america.net",
+  },
+  {
+    id: 4,
+    name: "Rizki Mardian",
+    username: "Cak Mardigu",
+    email: "Vokalisngutang@blue.band",
+  },
+  {
+    id: 5,
+    name: "Nailul Aborsi",
+    username: "Ustad binal",
+    email: "terorUmat@bahaya.co",
+  },
+  {
+    id: 6,
+    name: "Nendra Imsawan",
+    username: "Bendrak",
+    email: "Ridwan@surga.info",
+  },
+  {
+    id: 7,
+    name: "Boni akbar",
+    username: "Annisa ku",
+    email: "gapunya@mail.biz",
+  },
+  {
+    id: 8,
+    name: "Andri S",
+    username: "Andri HoHo",
+    email: "Temanbaik@edi.me",
+  },
+  {
+    id: 9,
+    name: "Gilang",
+    username: "Gilang Pusdipa",
+    email: "smkjaya@pusdipa.io",
+  },
+  {
+    id: 10,
+    name: "Vivi Rahmansyah",
+    username: "Vivi Ngerik",
+    email: "UNbringas@sangar.biz",
+  },
+];
